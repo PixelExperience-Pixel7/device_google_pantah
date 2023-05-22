cece71fa7de8322e2a106443c58c722d8bea1d70
lahaina <nattolecats@gmail.com>
2023-05-22 17:56:24 +0900

    pantah: Bring up PixelExperience

--- device-custom.mk
+++ device-custom.mk
@@ -5,14 +5,11 @@
 #
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-aosp
 
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
-
-# PowerShare
-include hardware/google/pixel/powershare/device.mk
 
 # Build necessary packages for vendor
 
